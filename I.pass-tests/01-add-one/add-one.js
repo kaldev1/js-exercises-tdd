1a4d3a4659ddf35501caab5954a684bb580b61fa
kaldev <jozsef.kalinak@gmail.com>
2020-05-22 19:37:26 +0200

    I.1

--- I.pass-tests/01-add-one/add-one.js
+++ I.pass-tests/01-add-one/add-one.js
@@ -1,1 +1,8 @@
-module.exports = function(numbers) {};
+module.exports = function(numbers) {
+    const result = []
+    // for (let i=0;i<numbers.length;i++) {
+    //     result.push(numbers[i]+1);
+    // };
+    numbers.forEach((item) => result.push(item+1) )
+    return result
+};
