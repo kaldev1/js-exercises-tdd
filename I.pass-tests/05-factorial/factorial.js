aea3d60c736fc8c4963320836a765cfc01e5c9ed
kaldev <jozsef.kalinak@gmail.com>
2020-05-22 20:27:14 +0200

    I.5

--- I.pass-tests/05-factorial/factorial.js
+++ I.pass-tests/05-factorial/factorial.js
@@ -8,6 +8,14 @@
 // calculate and return the factorial of int
 // note: factorial of 0 is 1
 
-function factorial(int) {}
+function factorial(int) {
+    let result = 1;
+    if (int>0) {
+        for (let i=1;i<int+1;i++) {
+            result*=i;
+        };
+    };
+    return result; 
+}
 
 module.exports = factorial;
